03ecd0367443c28e4df568aff70972076d76f464
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-10 23:54:54 +0100

    Create Login/SignUp page

---
+++ App/LoginSignupChoicePage.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; //Arrow icon
+
+const LoginSignupChoicePage = () => {
+  return (
+    <ImageBackground
+      source={require('../assets/background.png')} //photo as background 
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Ionicons name="arrow-back" size={30} color="#B2DDF9" style={styles.backIcon} />
+        <Text style={styles.title} >You have an account?</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Don’t have an account?</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
+//Style for the Login/Signup Choice Page
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  backIcon: {
+    position: 'absolute',
+    top: 50,
+    left: 20,
+  },
+  title: {
+    color: '#B2DDF9', // Standard font color..
+    marginVertical: 10,
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  button: {
+    backgroundColor: '#424242', // Standard buttons color..
+    borderRadius: 25,
+    paddingVertical: 15,
+    paddingHorizontal: 60,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#B2DDF9', 
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default LoginSignupChoicePage;
