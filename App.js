bea2aae9b1f7ab3bb0bbc9f224163f81fcb1f9e8
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-11 13:50:06 +0100

    Update App.js
    Edit app page

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import LoginSignupChoicePage from './App/LoginSignupChoicePage'; './App/LoginSignupChoicePage.js'; //Import the RegisterScreen
+import { NavigationContainer } from '@react-navigation/native'; // For managing navigation
+import { createStackNavigator } from '@react-navigation/stack'; // For creating a navigation stack
+import LoginSignupChoiceScreen from './src/pages/LoginSignupChoiceScreen';  //Add Login/Signup choice 
+import LoginScreen from './src/pages/LoginScreen'; // Add Login screen
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <LoginSignupChoicePage />; 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginSignupChoiceScreen">
+        {/* Login/Signup choice screen */}
+        <Stack.Screen
+          name="LoginSignupChoiceScreen"
+          component={LoginSignupChoiceScreen}
+          options={{ headerShown: false }} // Hide the top header
+        />
+        {/* Login page */}
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
