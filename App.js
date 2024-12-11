242f3e25c1d0434c3fd1604c4ce3470b931d1f43
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-12-11 14:23:11 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import LoginSignupChoicePage from './App/LoginSignupChoicePage'; './App/LoginSignupChoicePage.js'; //Import the RegisterScreen
 import TermsAndConditionsPage from './App/TermsAndConditions';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 export default function App() {
