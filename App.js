cbddf4772c367778ae5bc0f8e735600c87d05262
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-12-11 14:22:10 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import LoginSignupChoicePage from './App/LoginSignupChoicePage'; './App/LoginSignupChoicePage.js'; //Import the RegisterScreen
+import TermsAndConditionsPage from './App/TermsAndConditions';
+const Stack = createStackNavigator();
 
 export default function App() {
   return <LoginSignupChoicePage />; 
+  return (
+    <NavigatorContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TermsConditions" component={TermsAndConditionsPage} />
+        </Stack.Navigator>
+    </NavigatorContainer>
+  )
 }
+
+
