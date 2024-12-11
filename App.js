e3fe03743b18349d83d31e8d1d4f019aaf8be688
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-12-11 14:56:42 +0100

    TermsAndConditions created

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+
+
+import FrontPage from "./src/pages/FrontPage";
+
+import { NavigationContainer } from '@react-navigation/native'; // For managing navigation
+import { createStackNavigator } from '@react-navigation/stack'; // For creating a navigation stack
+import LoginSignupChoiceScreen from './src/pages/LoginSignupChoiceScreen';  //Add Login/Signup choice 
+import LoginScreen from './src/pages/LoginScreen'; // Add Login screen
+
 
 import LoginSignupChoicePage from './App/LoginSignupChoicePage'; // Pas dit pad aan naar jouw bestand
 import HomePage from './App/Homepage'; // Zorg ervoor dat je een HomePage hebt
@@ -10,8 +17,12 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginSignupChoicePage">
-      <Stack.Screen
+      <Stack.Navigator
+        initialRouteName="FrontPage"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="FrontPage" component={FrontPage} />
+        <Stack.Screen
           name="LoginSignupChoicePage"
           component={LoginSignupChoicePage}
           options={{ headerShown: false }} // Verberg de header
---
+++ src/pages/FrontPage.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image, ImageBackground } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+
+const FrontPage = () => {
+  const navigation = useNavigation();
+
+  return (
+    <ImageBackground
+      source={require("../assets/background.png")} //background page
+      style={styles.container}
+    >
+      <View style={styles.overlay}>
+      <Image
+          source={require('../assets/FrontLogo.png')} //FrontLogo
+          style={styles.logo}
+             resizeMode="contain"
+        />
+        <Text style={styles.title}>Parking made simple in real-time</Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("LoginSignupChoicePage")} // Navigate to LoginSignupPageChoicePage
+        >
+          <Text style={styles.arrow}>&rarr;</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    width: "100%",
+    height: "100%",
+    paddingTop:100,
+  },
+  logo: {
+    width: 250,
+    height: 250, 
+    marginBottom: 60,
+ opacity:0.8,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#B2DDF9",
+    textAlign: "center",
+    marginBottom: 60,
+    lineHeight: 40,
+    opacity:0.8,
+    width: "80%",
+  },
+
+  button: {
+    backgroundColor: "#424242", //Standard button color..
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    opacity:0.8,
+  },
+  arrow: {
+    fontSize: 36,
+    color: "#B2DDF9",
+  },
+});
+
+export default FrontPage;
