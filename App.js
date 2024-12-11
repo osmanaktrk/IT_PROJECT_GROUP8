823ba80aa13521b4a5d05297b2a38f9389ffa52f
Weiam1 <145970015+Weiam1@users.noreply.github.com>
2024-12-11 13:55:40 +0100

    Frontpage
    Front page with arrow to the LoginSignupChoisePage

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import LoginSignupChoicePage from './App/LoginSignupChoicePage'; './App/LoginSignupChoicePage.js'; //Import the RegisterScreen
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import FrontPage from "./src/pages/FrontPage";
+import LoginSignupChoicePage from "./src/pages/LoginSignupChoicePage";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <LoginSignupChoicePage />; 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="FrontPage"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="FrontPage" component={FrontPage} />
+        <Stack.Screen
+          name="LoginSignupChoicePage"
+          component={LoginSignupChoicePage}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
