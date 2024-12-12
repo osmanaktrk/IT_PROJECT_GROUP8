0c5ef917cb81aea1c6bbb00818765bfc9bdccfdf
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-12 11:37:45 +0100

    Update ForgetPasswordScreen
    Arrange the page as required and apply the style.

--- App/ForgetPasswordScreen.js
+++ App/ForgetPasswordScreen.js
@@ -1,24 +1,105 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 
-const ForgetPasswordScreen = () => {
+import React from 'react';
+import {View,Text,TextInput,TouchableOpacity,StyleSheet,ImageBackground,} from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Add Icons
+
+const ForgotPasswordScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is the Forget Password Screen</Text>
-    </View>
+    <ImageBackground
+      source={require('../assets/background.png')} 
+      style={styles.background}
+    >
+      <View style={styles.container}>
+      
+        {/* Title */}
+        <Text style={styles.title}>Forgot Password</Text>
+
+        {/* Subtitle */}
+        <Text style={styles.subtitle}>
+          Please enter your email to reset the password
+        </Text>
+
+        {/* Email input */}
+        <Text style={styles.label}>Your Email</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Joh.doe@gmail.com"
+            placeholderTextColor="#B0BEC5"
+            style={styles.input}
+          />
+        </View>
+
+        {/* Rest password button*/}
+        <TouchableOpacity style={styles.resetButton}>
+          <Text style={styles.resetButtonText}>Rest Password</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 20,
   },
-  text: {
+  backIcon: {
+    position: 'absolute',
+    top: 50,
+    left: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#B2DDF9",
+    textAlign: "center",
+    marginBottom: 30,
+    lineHeight: 50,
+    opacity:0.8,
+    width: "80%",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#B2DDF9',
+    textAlign: 'left',
+    marginBottom: 30,
+  },
+  label: {
+    fontSize: 16,
+    color: '#B2DDF9',
+    marginBottom: 10,
+  },
+  inputContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    height: 50,
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    fontSize: 16,
+    color: '#000',
+  },
+  resetButton: {
+    backgroundColor: '#424242',
+    borderRadius: 25,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  resetButtonText: {
+    color: '#B2DDF9', 
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
-export default ForgetPasswordScreen;
+export default ForgotPasswordScreen;
