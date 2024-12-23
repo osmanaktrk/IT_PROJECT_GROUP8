f26ec1df4602331806707f7de7a923812daa5460
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-24 00:14:39 +0100

    Update HistoryScreen.js
    Added soft delete functionality to manage user history records using an isDeleted field in Firestore

--- App/HistoryScreen.js
+++ App/HistoryScreen.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
 import { Swipeable } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
-import { collection, query, where, getDocs, addDoc, onSnapshot, deleteDoc, doc } from "firebase/firestore";
+import { collection, query, where, getDocs, addDoc, onSnapshot, deleteDoc,updateDoc, doc } from "firebase/firestore";
 import { firestoreDB } from '../FirebaseConfig'; 
 import { getAuth } from "firebase/auth";
 
@@ -66,7 +66,7 @@ export default function HistoryPage({ navigation }) {
   useEffect(() => {
     if (userID) {
       const historyRef = collection(firestoreDB, "history");
-      const q = query(historyRef, where("userID", "==", userID));
+      const q = query(historyRef, where("userID", "==", userID), where("isDeleted", "==", false));
 
     
       const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -84,7 +84,12 @@ export default function HistoryPage({ navigation }) {
   // Delete a history entry by ID
   const handleDelete = async (id) => {
     try {
-      await deleteDoc(doc(firestoreDB, "history", id)); 
+      
+      const docRef=doc(firestoreDB, "history", id); 
+      await updateDoc(docRef, { isDeleted: true }); 
+
+      // Update the local state to remove the deleted item
+      setHistoryData((prevHistory) => prevHistory.filter((item) => item.id !== id));
     } catch (error) {
       console.error("Error deleting document: ", error); 
     }
